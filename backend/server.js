64e2662aec741e90c2f3adda3214b6a1d8736963
dan patterson <starved14@hotmail.com>
2026-08-13 02:50:04 -0600

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,14 @@ import weatherRoutes from "./routes/weather.routes.js"
 import 'dotenv/config'; 
 import geoRoutes from "./routes/geoapify.routes.js"
 import morgan from 'morgan'
-import { Logtail } from "@logtail/node";
+// import { Logtail } from "@logtail/node";
 import tidesRoutes from "./routes/tides.routes.js"
 
 const app = express()
 
-const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN, {
-    endpoint: `https://${process.env.LOGTAIL_INGESTING_HOST}`,
-});
+// const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN, {
+//     endpoint: `https://${process.env.LOGTAIL_INGESTING_HOST}`,
+// });
 
 // Middleware
 app.use(cors())
