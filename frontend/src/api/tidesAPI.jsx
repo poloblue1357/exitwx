0b66a7dcf1cb2086f2031aec4a2c308d0b9a1cae
dan patterson <starved14@hotmail.com>
2026-08-13 01:45:11 -0600

    testing tidesapi fix

--- frontend/src/api/tidesAPI.jsx
+++ frontend/src/api/tidesAPI.jsx
@@ -2,7 +2,9 @@
 
 export const fetchTidesData = async (lat, lon) => {
 
-    const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+    // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+
+    const API_URL = 'https://weatherapp-cp4l.onrender.com'
 
     const params = new URLSearchParams ({
         lat, 
